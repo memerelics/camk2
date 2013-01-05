52132ead54f786e8745f68c5115c4743837ff85f
Hash <takuya21hashimoto@gmail.com>
2013-01-05 21:53:56 +0900

    init -- 'be rails new . --skip-test-unit --skip-bundle'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camk2::Application.config.secret_token = 'a4f737b1061f07c4310b1e34cba0b0e9d1ef79c34e3bc7fd114dd1c3a33075b5914fe89209c27c45cb791f9d0c4344d54f5f91fc5225eaa4811a0cb91424d20f'
