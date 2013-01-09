0e6686dd2b7c26e8c8740e66ad51ae4ebf7f6f36
Hash <takuya21hashimoto@gmail.com>
2013-01-09 14:11:38 +0900

    EvernoteApi model with basic specs

---
+++ app/models/evernote_api.rb
@@ -0,0 +1,54 @@
+# coding: utf-8
+class EvernoteApi
+
+  attr_accessor :token, :notestore
+
+  def initialize(user, mode=:sandbox)
+
+    evernoteHost = case mode
+                   when :sandbox then "sandbox.evernote.com"
+                   when :production then "www.evernote.com"
+                   end
+
+    userStoreUrl = "https://#{evernoteHost}/edam/user"
+    userStoreTransport = Thrift::HTTPClientTransport.new(userStoreUrl)
+    userStoreProtocol = Thrift::BinaryProtocol.new(userStoreTransport)
+    userStore = Evernote::EDAM::UserStore::UserStore::Client.new(userStoreProtocol)
+
+
+    versionOK = userStore.checkVersion("Evernote EDAMTest (Ruby)",
+                                        Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR,
+                                        Evernote::EDAM::UserStore::EDAM_VERSION_MINOR)
+    exit(1) if (!versionOK)
+
+    @token = user.token
+    noteStoreUrl = userStore.getNoteStoreUrl(token)
+
+    noteStoreTransport = Thrift::HTTPClientTransport.new(noteStoreUrl)
+    noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
+    @notestore = Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
+  end
+
+
+  ######################################################################
+  ## wrappers of raw Evernote APIs
+  ######################################################################
+  def notebooks
+    self.notestore.listNotebooks(self.token)
+  end
+
+  def notebook_named(name)
+    notebooks.select{|nb| nb.name == name }.first
+  end
+
+  def notes_in_a_notebook(name)
+    notebook = notebook_named(name)
+    return nil if notebook.nil?
+    f = Evernote::EDAM::NoteStore::NoteFilter.new
+    f.notebookGuid = notebook.guid
+    # findNotesはメタ情報(startIndex, totalNotes, updateCount(?))付きの検索結果を返す.
+    notelist = self.notestore.findNotes(self.token, f, 0, 10) # TODO: tweak offset and limit
+    return notelist.notes
+  end
+
+end
---
+++ spec/models/evernote_api_spec.rb
@@ -0,0 +1,37 @@
+# coding: utf-8
+require 'spec_helper'
+
+# RSpecでモックを使って外部サービスに依存するコードのテストを書いてみた - ぬいぐるみライフ(仮)
+# http://d.hatena.ne.jp/mickey24/20100302/1267464477
+describe EvernoteApi do
+  # EvernoteApi.newで通信させたくない => EvernoteApi.newをstubする?
+  # いや, initialize内で使ってるAPI通信をすべてstubすればいいのか.
+  before do
+    @dummy_user = mock(User.new)
+    @dummy_user.stub!(:token).and_return('dummy_token')
+
+    Thrift::HTTPClientTransport.stub!(new: nil)
+    Thrift::BinaryProtocol.stub!(new: nil)
+
+    us = mock(Evernote::EDAM::UserStore::UserStore::Client)
+    Evernote::EDAM::UserStore::UserStore::Client.stub!(new: us)
+    us.stub!(checkVersion: true,
+             getNoteStoreUrl: 'http://note_store_url/')
+  end
+
+  # 暗黙的にEvernoteApiクラスのインスタンスがsubjectになる.
+  # newに引数を渡したい時は明示的にsubjectを指定
+  # ref: http://blog.davidchelimsky.net/2012/05/13/spec-smell-explicit-use-of-subject/
+  describe 'EvernoteApi.new' do
+    subject { EvernoteApi.new(@dummy_user) }
+
+    # attributes
+    it { should respond_to(:notestore) }
+    it { should respond_to(:token) }
+    its(:token) { should eq 'dummy_token' }
+
+    # instance methods
+    it { should respond_to(:notebooks) }
+    it { should respond_to(:notes_in_a_notebook) }
+  end
+end
