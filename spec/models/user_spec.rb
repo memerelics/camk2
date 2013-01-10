4108cf2ad17a15be224b0926a2a9ba5467f772c4
Hash <takuya21hashimoto@gmail.com>
2013-01-11 06:00:48 +0900

    User model basic fields of *able in devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "*able" do
+    subject { User.new }
+
+    # trackable fields
+    it { should respond_to(:last_sign_in_at) }
+    it { should respond_to(:current_sign_in_at) }
+    it { should respond_to(:last_sign_in_ip) }
+    it { should respond_to(:current_sign_in_ip) }
+    it { should respond_to(:sign_in_count) }
+  end
 end
