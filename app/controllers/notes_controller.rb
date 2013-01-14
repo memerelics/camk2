de778e0f4b816c78c7a83bbd069a416846972cc2
Hash <takuya21hashimoto@gmail.com>
2013-01-14 23:23:50 +0900

    remove new/edit/destory actions and views from notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -24,66 +24,6 @@ class NotesController < ApplicationController
     end
   end
 
-  # GET /notes/new
-  # GET /notes/new.json
-  def new
-    @note = Note.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @note }
-    end
-  end
-
-  # GET /notes/1/edit
-  def edit
-    @note = Note.find(params[:id])
-  end
-
-  # POST /notes
-  # POST /notes.json
-  def create
-    @note = Note.new(params[:note])
-
-    respond_to do |format|
-      if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
-        format.json { render json: @note, status: :created, location: @note }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /notes/1
-  # PUT /notes/1.json
-  def update
-    @note = Note.find(params[:id])
-
-    respond_to do |format|
-      if @note.update_attributes(params[:note])
-        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /notes/1
-  # DELETE /notes/1.json
-  def destroy
-    @note = Note.find(params[:id])
-    @note.destroy
-
-    respond_to do |format|
-      format.html { redirect_to notes_url }
-      format.json { head :no_content }
-    end
-  end
-
   #   [OK] Evernote Serverにあるものを CaMK2へ
   # [TODO] CaMK2へ同期済だったが Evernote serverで削除されているもの
   def sync
