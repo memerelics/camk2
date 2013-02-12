e6c5c954a1fe04d2f17f6478059744d0d3124988
Hash <takuya21hashimoto@gmail.com>
2013-02-13 04:07:39 +0900

    refactoring -- extract tag method from EverNoteApi to Note model

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -12,9 +12,9 @@ describe Note do
                      :contentHash => "S\x11eAL9\xB4\xAB\xFF\xC1\x8E6]{\xEA*",
                      :content => "コンテンツ".force_encoding('ASCII-8BIT'),
                      :tagGuids => [dummy_guid, dummy_guid] )
-    @evernote = mock(EvernoteApi)
-    @evernote.stub!(extract_tagstrings: ["tagA","tagB","tagC"])
+
   end
+
 
   describe 'validations' do
     it "has a valid factory" do
@@ -32,9 +32,6 @@ describe Note do
     it "is invalid without title" do
       FactoryGirl.build(:note, title: nil).should_not be_valid
     end
-    it "allow blank stags" do
-      FactoryGirl.build(:note, stags: nil).should be_valid
-    end
   end
 
   describe ".store" do
@@ -42,7 +39,9 @@ describe Note do
       Note.stub!(:get_fullnotes).and_return([@efullnote])
       user = mock(User)
       user.stub!(id: 1)
-      Note.store("dummynotes", @evernote, user)
+      evernote = mock(EvernoteApi)
+      evernote.stub!(get_tag_names: ["tagA","tagB","tagC"])
+      Note.store("dummynotes", evernote, user)
     end
     it { @efullnote.title.encoding.name.should eq "ASCII-8BIT" }
     it { @efullnote.content.encoding.name.should eq "ASCII-8BIT" }
@@ -55,32 +54,36 @@ describe Note do
   end
 
   describe ".update_with_fullnote" do
-    describe "there's no guid-related note in DB" do
-      before { Note.destroy_all }
-      it {expect{Note.update_with_fullnote(@efullnote, @evernote)}.to \
-        raise_error(ActiveRecord::RecordNotFound) }
+    before do
+      Note.stub!(extract_tagstrings: 'same')
+      note = FactoryGirl.create(:note, guid: "abcd", content_hash: Digest::SHA1.hexdigest("xyz"))
+      note.stub!(stags: 'same')
+      @fullnote = mock(Evernote::EDAM::Type::Note)
     end
-    describe "has same content_hash" do
+    context "there's no guid-related note in DB" do
+      it "raise" do 
+        Note.destroy_all
+        expect{Note.update_with_fullnote(@efullnote)}.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+    context "has same content_hash" do
       before do
-        FactoryGirl.create(:note, guid: "abcd", content_hash: Digest::SHA1.hexdigest("xyz"))
-        @fullnote = mock(Evernote::EDAM::Type::Note)
-        @fullnote.stub!(guid: "abcd", contentHash: "xyz", title: "hoge", content: "hoge")
+        @fullnote.stub!(guid: "abcd", contentHash: "xyz", title: "hoge", content: "hoge", tagGuids: [dummy_guid, dummy_guid])
       end
       it "should not update" do
         expect {
-          Note.update_with_fullnote(@fullnote, @evernote)
+          Note.update_with_fullnote(@fullnote)
         }.to_not change{ Note.first.content_hash }
       end
     end
-    describe "has different content_hash" do
+
+    context "has different content_hash" do
       before do
-        FactoryGirl.create(:note, guid: "abcd", content_hash: Digest::SHA1.hexdigest("xyz"))
-        @fullnote = mock(Evernote::EDAM::Type::Note)
         @fullnote.stub!(guid: "abcd", contentHash: "xyz2", title: "hoge", content: "hoge", tagGuids: [dummy_guid, dummy_guid])
       end
       it "should update" do
         expect {
-          Note.update_with_fullnote(@fullnote, @evernote)
+          Note.update_with_fullnote(@fullnote)
         }.to change{ Note.first.content_hash }.
           from(Digest::SHA1.hexdigest("xyz")).to(Digest::SHA1.hexdigest("xyz2"))
       end
