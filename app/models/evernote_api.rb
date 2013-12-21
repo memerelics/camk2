7dce9cc1a91ee025a04e774e380c9eac78957be7
Hash <takuya21hashimoto@gmail.com>
2013-12-22 06:40:46 +0900

    OOP like wrapper for Evernote::EDAM apis

--- app/models/evernote_api.rb
+++ app/models/evernote_api.rb
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 class EvernoteApi
+  class InvalidVersion < StandardError; end
   class LostAuth < StandardError; end
   class NotebookNotFound < StandardError; end
   class TagNotFound < StandardError; end
+
+  DEFAULT_LIMIT = 10
 
   attr_accessor :token, :notestore
 
@@ -16,7 +19,7 @@ class EvernoteApi
     versionOK = userStore.checkVersion("Evernote EDAMTest (Ruby)",
                                         Evernote::EDAM::UserStore::EDAM_VERSION_MAJOR,
                                         Evernote::EDAM::UserStore::EDAM_VERSION_MINOR)
-    exit(1) if (!versionOK)
+    raise InvalidVersion unless versionOK
 
     noteStoreUrl = userStore.getNoteStoreUrl(token)
 
@@ -28,24 +31,74 @@ class EvernoteApi
   end
 
 
-  ######################################################################
   ## wrappers of raw Evernote APIs
   ######################################################################
-  def notebooks
-    self.notestore.listNotebooks(self.token)
+  # TODO: published filter
+  def notebooks(options={})
+    list_notebooks = self.notestore.listNotebooks(self.token)
+    return list_notebooks if options.empty?
+    if options[:name] # TODO: allow regexp
+      return list_notebooks.select{|nb| nb.name == options[:name] }
+    end
+    list_notebooks
   end
 
-  def notebook_named(name)
-    notebooks.select{|nb| nb.name == name }.first || raise(NotebookNotFound)
+  def notebook(options={})
+    list_notebooks = self.notestore.listNotebooks(self.token)
+    return list_notebooks.first if options.empty?
+    if options[:name]
+      return list_notebooks.find{|nb| nb.name == options[:name] } || raise(NotebookNotFound)
+    end
+    list_notebooks.first
   end
 
+  def notes(options={})
+    offset = options.delete(:offset) || 0
+    limit  = options.delete(:limit)  || DEFAULT_LIMIT
+    self.notestore.findNotes(self.token, build_filter(options), offset, limit)
+  end
+
+  # filter.methods.grep(/=/)
+  # :order=, :ascending=, :words=, :notebookGuid=, :tagGuids=, :timeZone=, :inactive=, :emphasized=
+  def build_filter(options={})
+    filter = Evernote::EDAM::NoteStore::NoteFilter.new
+    if options[:notebook]
+      filter.notebookGuid = options[:notebook].is_a?(Evernote::EDAM::Type::Notebook) ? options[:notebook].guid : options[:notebook]
+    end
+    if options[:tag]
+      filter.tagGuids = [(options[:tag].is_a?(Evernote::EDAM::Type::Tag) ? options[:tag].guid : options[:tag])]
+      #elsif options[:tags]
+      #filter.tagGuids =
+    end
+    filter
+  end
+
+  def tags(options={})
+    list_tags = self.notestore.listTags(self.token)
+    return list_tags if options.empty?
+    if options[:name]
+      return list_tags.select{|t| t.name == options[:name] }
+    end
+    list_tags
+  end
+
+  def tag(options={})
+    list_tags = self.notestore.listTags(self.token)
+    return list_tags.first if options.empty?
+    if options[:name]
+      return list_tags.find{|t| t.name == options[:name] } || raise(TagNotFound)
+    end
+    list_tags.first
+  end
+
+  # It'll be deprecated. use Evernote::EDAM::Type::Notebook#notes
   def notes_in_a_notebook(name)
     notebook = notebook_named(name)
     return nil if notebook.nil?
     f = Evernote::EDAM::NoteStore::NoteFilter.new
     f.notebookGuid = notebook.guid
     # findNotesはメタ情報(startIndex, totalNotes, updateCount(?))付きの検索結果を返す.
-    notelist = self.notestore.findNotes(self.token, f, 0, 10) # TODO: tweak offset and limit
+    notelist = self.notestore.findNotes(self.token, f, 0, DEFAULT_LIMIT)
     return notelist.notes
   end
 
@@ -58,7 +111,10 @@ class EvernoteApi
 
 end
 
-# APIをwrapしたクラスを新しく定義したいところ
+
+################################################################################
+# Extend Evernote::EDAM classes
+################################################################################
 class Evernote::EDAM::Type::Note
   def tag_names(evernote)
     return [] if self.tagGuids.nil?
@@ -66,3 +122,8 @@ class Evernote::EDAM::Type::Note
   end
 end
 
+class EvernoteApi::Notebook < Evernote::EDAM::Type::Notebook
+  # TODO: store token to call findNotes inside it. Need to wrap the class.
+  # def notes(options={})
+  # end
+end
